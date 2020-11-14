14160cce2c4101144ec439a67f541ac4323295ee
helenzhang8 <suki22889@gmail.com>
2020-11-14 01:08:30 -0500

    react-create-app on Heroku

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './React.css'
+import ReactList from './ReactList.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {Paper} from '@material-ui/core'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  render() {
+    return(
+      <div>
+      <h1>fruits make me emotional</h1>
+        <Paper>
+          <ReactList/>
+        </Paper>
+        <h4>made by helen</h4>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
